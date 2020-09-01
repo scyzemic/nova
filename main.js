56d6227be0d4e6525e05cbe5128e80b1f844019a
GabeCodes <gabriel.2m.deleon@gmail.com>
2020-09-01 16:22:42 -0400

    added .user-info command
    posts username and id of commander

--- main.js
+++ main.js
@@ -16,6 +16,10 @@ client.on('message', (message) => {
 		message.channel.send(
 			`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`,
 		);
+	} else if (message.content === `${prefix}user-info`) {
+		message.channel.send(
+			`Your username: ${message.author.username}\nYour ID: ${message.author.id}`,
+		);
 	}
 });
 
