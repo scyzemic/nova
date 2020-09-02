98ab2f91e2cafcd508ff0b0cf85dea424bd17a02
GabeCodes <gabriel.2m.deleon@gmail.com>
2020-09-02 12:43:49 -0400

    basic argument setup
    !careful. Ln 17 breaks Ln25

--- main.js
+++ main.js
@@ -12,18 +12,36 @@ client.once('ready', () => {
 
 // Listen for 'message' event
 client.on('message', (message) => {
+	// exit early if message doesn't start with the prefix
+	// or the message author is a bot (so we don't have infinite loops)
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	// parse the message content, list of string w/o the prefix
+	const args = message.content.slice(prefix.length).trim().split(' ');
+	// parse the command word from the list
+	// TODO - find a way to do this without side effects
+	const command = args.shift().toLocaleLowerCase();
+
 	if (message.content.match(/[hH]ow fly is/)) {
 		message.channel.send(
 			"they're so fly that when Jim Jones sees them, he goes, BALLIIINNNN!!!",
 		);
-	} else if (message.content === `${prefix}server`) {
+	} else if (command === 'server') {
 		message.channel.send(
 			`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`,
 		);
-	} else if (message.content === `${prefix}user-info`) {
+	} else if (command === 'user-info') {
 		message.channel.send(
 			`Your username: ${message.author.username}\nYour ID: ${message.author.id}`,
 		);
+	} else if (command === 'args-info') {
+		if (!args.length) {
+			return message.channel.send(
+				`You didn't provide any arguments, ${message.author}!`,
+			);
+		}
+
+		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
 	}
 });
 
