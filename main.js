d82fdb20e7e8ecfed4598e55e543a223a10b5042
GabeCodes <gabriel.2m.deleon@gmail.com>
2020-09-03 10:44:00 -0400

    Dynamic command execution
    Execute commands dynamically so we don't have a bunch of overhead and people can more easily add commands :)

--- main.js
+++ main.js
@@ -25,40 +25,27 @@ client.once('ready', () => {
 
 // Listen for 'message' event
 client.on('message', (message) => {
-	// exit early if message doesn't start with the prefix
-	// or the message author is a bot (so we don't have infinite loops)
 	const fanFavorite = message.content.match(/[hH]ow fly (is|are)/);
 	const isValidCommand = fanFavorite || message.content.startsWith(prefix);
+
 	if (!isValidCommand || message.author.bot) return;
 
-	// parse the message content, list of string w/o the prefix
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	// parse the command word from the list
-	// TODO - find a way to do this without side effects
 	const command = args.shift().toLocaleLowerCase();
 
 	if (fanFavorite) {
-		message.channel.send(
+		return message.channel.send(
 			"they're so fly that when Jim Jones sees them, he goes, BALLIIINNNN!!!",
 		);
-	} else if (command === 'ping') {
-		client.commands.get('ping').execute(message, args);
-	} else if (command === 'server') {
-		client.commands.get('server').execute(message, args);
-	} else if (command === 'user-info') {
-		client.commands.get('user-info').execute(message, args);
-	} else if (command === 'so-fly') {
-		client.commands.get('so-fly').execute(message, args);
-	} else if (command === 'kick') {
-		client.commands.get('kick').execute(message, args);
-	} else if (
-		(command === 'prune' &&
-			message.guild.member(message.author).roles.highest.name === 'Owner') ||
-		message.guild.member(message.author).roles.highest.name === 'Admin'
-	) {
-		client.commands.get('prune').execute(message, args);
-	} else if (command === 'gift') {
-		client.commands.get('gift').execute(message, args);
+	}
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command');
 	}
 });
 
