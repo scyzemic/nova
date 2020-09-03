70b3d6ab861c4a87a71cc1ee4ec945e7938ff294
GabeCodes <gabriel.2m.deleon@gmail.com>
2020-09-03 14:32:21 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -18,7 +18,7 @@ module.exports = {
 			data.push(
 				`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`,
 			);
-
+			// test
 			return message.author
 				.send(data, { split: true })
 				.then(() => {
