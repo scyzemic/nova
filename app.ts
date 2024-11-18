dc793b51e8ef4585b58751f6e49acbd24b3d9e2e
Gabriel Matias <11906352+GMatias93@users.noreply.github.com>
2024-11-17 20:53:20 -0500

    working with oak

--- app.ts
+++ app.ts
@@ -1,67 +1,86 @@
-import express from "npm:express";
+import { Application, Context, Router, Status } from "jsr:@oak/oak";
 import {
   InteractionResponseType,
   InteractionType,
-  verifyKeyMiddleware,
+  verifyKey,
 } from "npm:discord-interactions";
 import { getRandomEmoji } from "./utils.ts";
-
-// Create an express app
-const app = express();
-// Get port, or default to 3000
-const PORT = Deno.env.get("PORT") || 3000;
 
 const PUBLIC_KEY = Deno.env.get("PUBLIC_KEY");
 if (!PUBLIC_KEY) {
   throw new Error("PUBLIC_KEY is not set");
 }
 
-/**
- * Interactions endpoint URL where Discord will send HTTP requests
- * Parse request body and verifies incoming requests using discord-interactions package
- */
-app.post(
-  "/interactions",
-  verifyKeyMiddleware(PUBLIC_KEY),
-  async function (req, res) {
-    // Interaction type and data
-    const { type, data } = req.body;
+const router = new Router();
 
-    /**
-     * Handle verification requests
-     */
+// /**
+//  * Interactions endpoint URL where Discord will send HTTP requests
+//  * Parse request body and verifies incoming requests using discord-interactions package
+//  */
+router.post(
+  "/interactions",
+  async (ctx: Context) => {
+    const signature = ctx.request.headers.get("X-Signature-Ed25519");
+    const timestamp = ctx.request.headers.get("X-Signature-Timestamp");
+    ctx.assert(
+      signature && timestamp,
+      Status.Unauthorized,
+      "Missing signature or timestamp",
+    );
+
+    // Verify request signature
+    const rawBody = await ctx.request.body.text();
+    const isValidRequest = await verifyKey(
+      rawBody,
+      signature,
+      timestamp,
+      PUBLIC_KEY,
+    );
+    ctx.assert(
+      isValidRequest,
+      Status.Unauthorized,
+      "Invalid request signature",
+    );
+
+    // Parse request body
+    const { type, data } = JSON.parse(rawBody);
+    ctx.assert(
+      type === InteractionType.PING ||
+        type === InteractionType.APPLICATION_COMMAND,
+      Status.BadRequest,
+      "Unknown interaction type",
+    );
+
     if (type === InteractionType.PING) {
-      return res.send({ type: InteractionResponseType.PONG });
+      ctx.response.body = { type: InteractionResponseType.PONG };
+      return;
     }
 
     /**
      * Handle slash command requests
      * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
      */
-    if (type === InteractionType.APPLICATION_COMMAND) {
-      const { name } = data;
+    const { name } = data;
+    ctx.assert(
+      name === "test",
+      Status.BadRequest,
+      `Unknown command: ${name}`,
+    );
 
-      // "test" command
-      if (name === "test") {
-        // Send a message into the channel where command was triggered from
-        return res.send({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            // Fetches a random emoji to send from a helper function
-            content: `hello world ${getRandomEmoji()}`,
-          },
-        });
-      }
-
-      console.error(`unknown command: ${name}`);
-      return res.status(400).json({ error: "unknown command" });
-    }
-
-    console.error("unknown interaction type", type);
-    return res.status(400).json({ error: "unknown interaction type" });
+    // Send a message into the channel where command was triggered from
+    ctx.response.body = {
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        // Fetches a random emoji to send from a helper function
+        content: `hello world ${getRandomEmoji()}`,
+      },
+    };
   },
 );
 
-app.listen(PORT, () => {
-  console.log("Listening on port", PORT);
-});
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port: 3000 });
